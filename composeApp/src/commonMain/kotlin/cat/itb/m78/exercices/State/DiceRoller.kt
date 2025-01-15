a3468057d9160c303475a0e812695f3b259d15c3
sergi.albalat.7e8 <sergi.albalat.7e8@itb.cat>
2025-01-15 13:03:34 +0100

    States acabados

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/State/DiceRoller.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/State/DiceRoller.kt
@@ -3,33 +3,99 @@ package cat.itb.m78.exercices.State
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.em
+import kotlinx.coroutines.launch
 import m78exercices.composeapp.generated.resources.Res
+import m78exercices.composeapp.generated.resources.dice_1
+import m78exercices.composeapp.generated.resources.dice_2
+import m78exercices.composeapp.generated.resources.dice_3
+import m78exercices.composeapp.generated.resources.dice_4
+import m78exercices.composeapp.generated.resources.dice_5
+import m78exercices.composeapp.generated.resources.dice_6
+import m78exercices.composeapp.generated.resources.empty_dice
 import m78exercices.composeapp.generated.resources.tapestry
 import m78exercices.composeapp.generated.resources.title
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
+import kotlin.random.Random
 
 @Composable
 fun DiceRoller(){
-    Image(
-        painter = painterResource(Res.drawable.tapestry),
-        contentDescription = null,
-        modifier = Modifier.fillMaxSize()
-    )
-    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxSize()) {
+    var dice by remember{ mutableStateOf(0) }
+    var dice2 by remember{ mutableStateOf(0) }
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+    ){ padding ->
         Image(
-            painter = painterResource(Res.drawable.title),
-            contentDescription = null
+            painter = painterResource(Res.drawable.tapestry),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize()
         )
-        Button(onClick = {}, modifier = Modifier.fillMaxWidth()){
-            Text("Roll the dice")
+        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(Res.drawable.title),
+                contentDescription = null
+            )
+            Button(onClick = {
+                dice = rollDice()
+                dice2 = rollDice()
+                if(dice == 6 && dice2 == 6){
+                    scope.launch {
+                        snackbarHostState.showSnackbar("JACKPOT!")
+                    }
+                }
+            }, modifier = Modifier.fillMaxWidth()){
+                Text("Roll the dice")
+            }
+            Row{
+                Dice(dice)
+                Dice(dice2)
+            }
         }
     }
+}
+
+@Composable
+fun Dice(dice : Int){
+    Image(
+        painter = painterResource(diceImage(dice)),
+        contentDescription = null
+    )
+}
+
+fun diceImage(diceNumber : Int) : DrawableResource{
+    when(diceNumber){
+        1 -> return Res.drawable.dice_1
+        2 -> return Res.drawable.dice_2
+        3 -> return Res.drawable.dice_3
+        4 -> return Res.drawable.dice_4
+        5 -> return Res.drawable.dice_5
+        6 -> return Res.drawable.dice_6
+    }
+    return Res.drawable.empty_dice
+}
+
+fun rollDice() : Int{
+    return Random.nextInt(1,7)
 }
